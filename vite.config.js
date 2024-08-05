61037789554de96e9da3bc5f23fcf7743a3a0c2b
jas112 <jas.sandmaster@gmail.com>
2024-08-04 20:23:24 -0700

    modifications to vite.config

--- vite.config.js
+++ vite.config.js
@@ -41,7 +41,7 @@ export default defineConfig({
     outDir: 'dist',
     assetsDir: 'assets',
     rollupOptions: {
-      input: './index.html', // Ensure this points to your index.html
+      input: './src/index.html', // Ensure this points to your index.html
     },
   },
   server:{port: 3000}
