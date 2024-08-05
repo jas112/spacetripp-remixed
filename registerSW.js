eb0077ad19303430f0b897d5868161cfba416c21
jas112 <jas.sandmaster@gmail.com>
2024-08-04 18:09:20 -0700

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/spacetripp-remixed/service-worker.js', { scope: '/spacetripp-remixed/' })})}
